6b1a5333bdc5057be419de5fa596a95a24367eef
Jonaxx8 <nikil.jonnada8@gmail.com>
2025-05-23 13:34:35 +0530

    minor redirection

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import Hero from "@/components/hero";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  return (
-    <>
-      <Hero />
-      
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        
-      </main>
-    </>
-  );
+  redirect("/sign-in");
 }
